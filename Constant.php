ab2b212abd11e7d557cf8991afca6aa1019e0db0
Farhan <56377400+ittggrgfthh@users.noreply.github.com>
2021-10-10 17:20:03 +0700

    Create Constant.php

---
+++ Constant.php
@@ -0,0 +1,15 @@
+<?php 
+
+//variabel konstan hanya bisa dibuat atau dideklarasikan sekali saja, disarankan dengan huruf besar semua
+define("AUTHOR", "Farhan Taqi Abdussalam"); // ("NAMA_VARIABEL", "Value");
+define("APP_VERSION", 100);
+//penghubung kalimat tidak disarankan menggunakan spasi
+
+echo "Author : ";
+echo AUTHOR;
+echo "\n";
+
+echo "App Version : ";
+echo APP_VERSION;
+echo "\n";
+?>
