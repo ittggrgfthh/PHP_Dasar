21da008b1af7d1c6f389410092308910caf4d6bb
Farhan <56377400+ittggrgfthh@users.noreply.github.com>
2021-10-10 19:47:13 +0700

    Create TipeDataNull.php

---
+++ TipeDataNull.php
@@ -0,0 +1,20 @@
+<?php 
+
+//Tipe data null bisa digunakan untuk mengosongkan variabel
+//case insensitive
+$nama = "Farhan";
+echo "Nama : ";
+echo $nama;
+echo "\n";
+
+$nama = null;
+echo "Nama : ";
+echo $nama;
+echo "\n";
+
+$umur = null;
+echo "Umur : ";
+echo $umur;
+echo "\n";
+//atau bisa untuk meng-set variabel menjadi kosong
+?>
