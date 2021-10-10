f9259bbd050ff104ee9ea769c688e450fdaa3e30
Farhan <56377400+ittggrgfthh@users.noreply.github.com>
2021-10-10 17:07:28 +0700

    Update Variabel.php

--- Variabel.php
+++ Variabel.php
@@ -17,4 +17,20 @@ echo "Nama :";
 echo $nama;
 echo "\n";
 //kita bisa mengganti tipe data dari string menjadi integer, yang mana di c++ atau java tidak bisa
+
+//Variable Variables
+//fitur ini kemungkinan hanya dimiliki php
+//tidak disarankan digunakan, kecuali jika perlu. karena fitur ini akan membuat aplikasi bingung
+
+$contoh = "farhan";
+$$contoh = "Taqi";
+
+echo "Contoh : ";
+echo $contoh;
+echo "\n";
+
+echo "farhan : ";
+echo $farhan;
+echo "\n";
+//fitur ini memungkinkan kita memanggil variabel$$ dengan value varibel$
 ?>
