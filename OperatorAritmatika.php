2a6403b5a50f734c3bd55ed6cf0fbd13fb0f5f89
Farhan <56377400+ittggrgfthh@users.noreply.github.com>
2021-10-11 05:36:23 +0700

    Create OperatorAritmatika.php

---
+++ OperatorAritmatika.php
@@ -0,0 +1,22 @@
+<?php 
+
+$a = 10;
+$b = 10;
+
+$hasil = $a + $b;
+var_dump($hasil);
+
+//mengubah hasil ke negatif
+$hasilNegatif = -$hasil;
+var_dump($hasilNegatif);
+
+//mengubah hasil ke positif
+$hasilPositif = +$hasil;
+var_dump($hasilPositif);
+
+//modulo
+$hasilModulo = 100 % 3;
+var_dump($hasilModulo);
+
+//operator lainnya silahkan coba sendiri karena ada banyak
+?>
