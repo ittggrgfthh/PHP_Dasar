603a270c7995866de688083d9badb8ac0347093c
Farhan <56377400+ittggrgfthh@users.noreply.github.com>
2021-10-11 06:12:11 +0700

    Create OperatorPenugasan.php

---
+++ OperatorPenugasan.php
@@ -0,0 +1,19 @@
+<?php
+
+$total = 0;
+
+$buah = 5000;
+$ayam = 10000;
+$jusJeruk = 5000;
+
+$total += $buah;
+$total += $ayam;
+$total += $jusJeruk;
+
+var_dump($total);
+/* operator penugasan adalah penyingkatan saja sebenarnya (just a shorcut) 
+   contohnya -=, *=, /=, %=
+   silahkan dicoba sendiri
+*/
+
+?>
