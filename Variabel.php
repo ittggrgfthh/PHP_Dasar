0194d0cfeacacdeacbb0c49c7da65f65c1406cf1
Farhan <56377400+ittggrgfthh@users.noreply.github.com>
2021-10-10 16:54:43 +0700

    Create Variabel.php

---
+++ Variabel.php
@@ -0,0 +1,20 @@
+<?php
+
+//cara membuat variabel dan menggunakannya
+$nama = "Farhan";
+echo "Nama : ";
+echo $nama;
+echo "\n";
+
+$umur = 19;
+echo "Umur : ";
+echo $umur;
+echo "\n";
+//didepan $tidak boleh ada spasi, penamaan variabel tidak boleh dengan spasi
+
+$nama = 20;
+echo "Nama :";
+echo $nama;
+echo "\n";
+//kita bisa mengganti tipe data dari string menjadi integer, yang mana di c++ atau java tidak bisa
+?>
