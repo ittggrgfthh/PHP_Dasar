8bb1c0b126110d3d19da3ccdb0072b854cac93fb
Farhan <56377400+ittggrgfthh@users.noreply.github.com>
2021-10-11 14:16:40 +0700

    Create OperatorPerbandingan.php

---
+++ OperatorPerbandingan.php
@@ -0,0 +1,11 @@
+<?php 
+
+//operator perbandingan memiliki value boolean
+var_dump("10" == 10);
+var_dump("10" === 10);
+
+var_dump(50 != 50);
+var_dump(9 <> 9); // <> sama seperti !=
+var_dump(9 > 9);
+//silahkan operator yang lain di coba sendiri
+?>
