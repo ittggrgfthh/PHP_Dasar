4c96a9263bed3b17f9544c6f0cd1b7a3ef449910
Farhan <56377400+ittggrgfthh@users.noreply.github.com>
2021-10-13 18:45:13 +0700

    Add files via upload

---
+++ Komentar.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ini adalah program untuk menampilkan string Halo menggunakan function
+*/
+function sayHello(){
+    //ini adalah komentar 1 paragraf
+    echo "Halo\n";
+}
+//komentar biasa digunakan sebagai dokumentasi karena semua yang didalam komentar akan diabaikan program
+?>
