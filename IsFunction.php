caa7c1ecec1039f5db58bcd12a78a4d0658f784a
Farhan <56377400+ittggrgfthh@users.noreply.github.com>
2021-10-14 06:47:54 +0700

    Add files via upload

---
+++ IsFunction.php
@@ -0,0 +1,12 @@
+<?php
+
+$data = "Farhan";
+
+var_dump(is_string($data));
+var_dump(is_int($data));
+var_dump(is_bool($data));
+var_dump(is_float($data));
+var_dump(is_array($data));
+var_dump(is_null($data));
+//view the documentation at : https://www.php.net/manual/en/ref.var.php
+?>
